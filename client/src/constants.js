d10dfbe860cf66842e953b02978883478be0dc8d
Roman Matveev <r.matveev@cek.ru>
2020-03-31 23:00:09 +0300

    Remove client

--- client/src/constants.js
+++
@@ -1,9 +0,0 @@
-const fieldUrl = 'http://localhost:2000/getFIeld';
-const resetFieldUrl = 'http://localhost:2000/resetField';
-const moveUrl = 'http://localhost:2000/move';
-
-module.exports = {
-  fieldUrl,
-  resetFieldUrl,
-  moveUrl
-} 
